4ed403ae317d5b0e429460f9a2046af10a57d704
Ankit Kumar <ankit.kumar2774y@gmail.com>
2024-01-09 23:54:36 +0530

    Env changes

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDQwsXj3H91rKNy5i2fE4kVbyDbSLQyDis",
-    authDomain: "photofolio-6a861.firebaseapp.com",
-    projectId: "photofolio-6a861",
-    storageBucket: "photofolio-6a861.appspot.com",
-    messagingSenderId: "760659941665",
-    appId: "1:760659941665:web:59268f4e5e7edfdd430660"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
