a48f292ccce3c1eab46656d03964429d1c3a2d93
Ankit Kumar <ankit.kumar2774y@gmail.com>
2024-01-10 13:49:33 +0530

    firebaseInit file changed from process.env config

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -6,13 +6,21 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID
+// };
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyDQwsXj3H91rKNy5i2fE4kVbyDbSLQyDis",
+    authDomain: "photofolio-6a861.firebaseapp.com",
+    projectId: "photofolio-6a861",
+    storageBucket: "photofolio-6a861.appspot.com",
+    messagingSenderId: "760659941665",
+    appId: "1:760659941665:web:59268f4e5e7edfdd430660"
 };
 
 // Initialize Firebase
